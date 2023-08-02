4f6aacf3006ff5bce5b39957d168991e2629067a
steven-kamanga <kamangasteven00@gmail.com>
2023-08-02 15:08:25 +0200

    pycodestyle checks

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -14,14 +14,14 @@ if __name__ == "__main__":
     r = requests.get("https://jsonplaceholder.typicode.com/todos/",
                      params={"userId": argv[1]})
     todos = r.json()
-    l = []
+    x = []
     dd = {}
     for t in todos:
         dd = {"task": t.get("title"),
               "completed": t.get("completed"),
               "username": user
               }
-        l.append(dd)
-    d = {argv[1]: l}
+        x.append(dd)
+    d = {argv[1]: x}
     with open("{}.json".format(argv[1]), "w") as f:
         json.dump(d, f)

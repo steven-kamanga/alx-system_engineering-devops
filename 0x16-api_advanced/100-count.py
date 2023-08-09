c6e0c1615d7ef0b29d501455b8ff900418bbd482
steven-kamanga <kamangasteven00@gmail.com>
2023-08-09 22:23:43 +0200

    ......

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" A module that queries the Reddit API"""
+import requests
+import sys
+after = None
+count_dic = []
+
+
+def count_words(subreddit, word_list):
+    """parses the title of all hot articles, and prints a sorted count of given
+    keywords (case-insensitive, delimited by spaces) """
+    global after
+    global count_dic
+    headers = {'User-Agent': 'xica369'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    response = requests.get(url, 
+                            allow_redirects=False,
+                            headers=headers, 
+                            params=parameters)

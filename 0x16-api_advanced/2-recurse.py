bb16fd9ffe75dcf3b55d6f26aaee84ece01c7b95
steven-kamanga <kamangasteven00@gmail.com>
2023-08-09 22:22:09 +0200

    ......

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,21 +7,21 @@ to the Reddit api and returns a list containing titles of hot articles for a pro
 If results are not found the function returns None.
 """
 
+import requests
+
+
 def recurse(subreddit, hot_list=[], after=None):
-    """
-    This function makes a query to the Reddit api and returns a list containing titles of hot articles for a provoded subreddit.
-    If results are not found the function returns None.
-    """
+    """queries Reddit API and returns list of titles of hot articles"""
     try:
         r = requests.get("https://www.reddit.com/r/{}/hot.json".
-                         format(subreddit), 
+                         format(subreddit),
                          params={"after": after},
-                         headers={"User-Agent": "custom"}, 
-                         allow_redirects=False
+                         allow_redirects=False,
+                         headers={"User-Agent": "custom"},
                          ).json()
     except:
         return None
-    
+
     if ("data" in r and "children" in r.get("data")):
         for i in r.get("data").get("children"):
             hot_list.append(i.get("data").get("title"))

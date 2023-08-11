cb7c1daa17af83179bbeb1885e8018d9a13cf09d
steven-kamanga <kamangasteven00@gmail.com>
2023-08-11 11:04:45 +0200

    not making commits

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,10 +8,7 @@ If results are not found the function returns None.
 
 def recurse(subreddit, hot_list=[], after=""):
     """
-    Queries the Reddit API and returns
-    a list containing the titles of all hot articles for a given subreddit.
-
-    - If not a valid subreddit, return None.
+    Same as above (:
     """
     req = requests.get(
         "https://www.reddit.com/r/{}/hot.json".format(subreddit),

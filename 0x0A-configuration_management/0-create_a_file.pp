67b85512ce2d7124f2d562f3e56981f5e87cdfcd
steven kamange <kamangasteven00@gmail.com>
2024-08-31 09:32:39 +0200

    linted

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
-file {
-    '/tmp/':
-    ensure => file,
-    path => '/tmp/school',
-    mode => '0744',
-    owner => 'www-data',
-    group => 'www-data',
-    content => 'I love Puppet'
+file { '/tmp/school':
+    ensure  => file,
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
 }
